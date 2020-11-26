2addb46c74a0c2320962a87102b5562d05313ff1
Farhan Mahmood <farhanmahmoodny@gmail.com>
2020-11-26 21:22:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,15 @@ def long_planeteer_calls(calls)
   false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.each do |snack|
+    cheese_types.each do |cheese|
+      if snack == cheese
+        return cheese
+      end
+    end
+  end
+  nil
 end
